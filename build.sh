6d69972ab4fc55c33fe3d8380d300e7ab02f696a
ittou <VYG07066@gmail.com>
2020-06-19 17:15:33 +0900

    image name bugfix

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 IPADDRESS=$(ip addr show $( netstat -rn | grep UG | awk -F' ' '{print $8}')\
     | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' \
     | grep -o [0-9].*)
+VER=2019.2
 docker image build \
     --rm \
     --build-arg IP=${IPADDRESS} \
-    --build-arg VITIS_VER=2019.2 \
+    --build-arg VITIS_VER=${VER} \
     --build-arg VITIS_MAIN=Xilinx_Vitis_2019.2_1106_2127.tar.gz \
     --no-cache \
-    -t vitis${VIVADO_VER} .
+    -t vitis${VER} .
